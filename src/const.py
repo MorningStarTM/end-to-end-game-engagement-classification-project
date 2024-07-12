eb823acf4a1fd7ff5a0f836f32685f98a8350612
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-12 12:31:07 +0530

    const file created

---
+++ src/const.py
@@ -0,0 +1,22 @@
+# constants.py
+
+# Database settings
+DB_NAME = "player.db"
+
+# Status codes
+STATUS_SUCCESS = 200
+STATUS_CREATED = 201
+STATUS_BAD_REQUEST = 400
+STATUS_NOT_FOUND = 404
+STATUS_INTERNAL_SERVER_ERROR = 500
+
+# Messages
+MESSAGE_PLAYER_CREATED = "Player created successfully."
+MESSAGE_PLAYER_UPDATED = "Player updated successfully."
+MESSAGE_PLAYER_DELETED = "Player deleted successfully."
+MESSAGE_PLAYERS_ADDED_FROM_CSV = "Players added from CSV successfully."
+MESSAGE_PLAYER_NOT_FOUND = "No player found with the given ID."
+MESSAGE_INTERNAL_SERVER_ERROR = "Internal server error."
+
+# Other constants
+CSV_FILE_PATH = "data\\online_gaming_behavior_dataset.csv"
